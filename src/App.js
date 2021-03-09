75250b633e2457a7bafcdf92d2caad772d96d18f
Gaétan Rousseau <rgaetan1711@hotmail.fr>
2021-03-09 15:11:56 +0100

    mon premier commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from './Header';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+
+         {/* Tinder Cards */}
+        {/* Buttons below tinder cards */}
+      
+        {/* Chats screen */}
+        {/* Individual chats screen */}    
+
+
+    </div>
+  );
+}
+        
+export default App;
